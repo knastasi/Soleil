3c50971ed8f197b1b71b8cc521afa36aa46273e3
Ana Nuno <nunoany@gmail.com>
2020-04-03 17:52:33 -0500

    Character controller, level jumpers, enemy follows, AI camera controller.
    I cant figure out the point click interaction, I'm having problems implementining this time. I'll continue to work on it.

---
+++ Soleil/Assets/Script/enemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+    public int health = 100;
+    public GameObject deatheffect;
+
+    public void takedamage(int damage)
+	{
+        health -= damage;
+        if (health <= 0)
+		{
+            death();
+		}
+	}
+
+    void death()
+	{
+        Instantiate(deatheffect, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+	}
+}
