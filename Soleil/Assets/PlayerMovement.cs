50f9162f0fb85fbaa1b0539c867cb9cb0d501fe5
Leslyanne Warrington <42988100+leslyannew@users.noreply.github.com>
2020-05-02 01:28:54 -0500

    Finish Final Submission

--- Soleil/Assets/PlayerMovement.cs
+++ Soleil/Assets/PlayerMovement.cs
@@ -14,7 +14,7 @@ public class PlayerMovement : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         Vector3 pos = transform.position;
 
